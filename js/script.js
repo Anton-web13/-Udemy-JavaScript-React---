c724aa91d423ab3b93041975a0253b54ef9aa9a5
Anton <example@gmail.com>
2022-03-15 23:18:07 +0300

    055 Promise (ES6)

--- js/script.js
+++ js/script.js
@@ -155,7 +155,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 50000);
+    // const modalTimerId = setTimeout(openModal, 5000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -582,5 +582,85 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
+
+
+    // console.log('Запрос данных...');
+
+    // setTimeout(() => {
+    //     console.log('Подготовка данных...');
+
+    //     const product = {
+    //         name: 'TV',
+    //         price: 2000
+    //     };
+
+    //     setTimeout(() => {
+    //         product.status = 'order';
+    //         console.log(product);
+    //     }, 2000);
+    // }, 2000);
+
+
+    console.log('Запрос данных...');
+
+    const req = new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            console.log('Подготовка данных...');
+    
+            const product = {
+                name: 'TV',
+                price: 2000
+            };
+    
+           resolve(product);
+
+        }, 2000);
+    });
+
+    // req.then((product) => {
+        // setTimeout(() => {
+        //     product.status = 'order';
+        //     console.log(product);
+        // }, 2000);
+    // });
+
+    req.then((product) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                product.status = 'order';
+                resolve(product);
+                // reject();
+            }, 2000);
+        });
+    }).then(data => {
+        data.modify = true;
+        return data;
+    }).then((data) => {
+        console.log(data);
+    }).catch(() => {
+        console.error('Произошла ошибка');
+    }).finally(() => {
+        console.log('Finally');
+    });
+    
+
+    const test = time => {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(), time);
+        });
+    };
+
+    // test(1000).then(() => console.log('1000 ms'));
+    // test(2000).then(() => console.log('2000 ms'));
+
+    Promise.all([test(1000), test(6000)]).then(() => {
+        console.log('All');
+    });
+
+    Promise.race([test(1000), test(6000)]).then(() => {
+        console.log('Race');
+    });
+
 
 });
